ccd84694b921d0bb99c3352381a6be6154abf697
Martinelllo <hellmann.martin@gmx.net>
2025-07-02 01:12:02 +0200

    Update installer files: Wed Jul  2 01:12:02 CEST 2025

---
+++ install/install-multi-plattform.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+sudo apt-get update -y
+sudo apt-get install git -y
+
+rm -rf ~/multi-platform/
+git clone https://github.com/Martinelllo/smarthome-multi-platform-installer.git ~/multi-platform
+rm -rf ~/multi-platform/.git
+rm -rf ~/multi-platform/.bin
+
+sudo apt-get install python3-pip -y
+pip3 install -r ~/multi-platform/requirements.txt --break-system-packages
+
+# install pigpiod
+sudo apt-get install pigpiod -y
+sudo systemctl enable pigpiod
+sudo systemctl start pigpiod
+
+# enable i2c on raspi-config
+sudo raspi-config nonint do_i2c 0
+
+# install i2c helper tools
+sudo apt-get install -y python3-smbus i2c-tools
+
+# install and start service
+cp ~/multi-platform/.bin/multi_module_platform.service etc/systemd/system/multi_module_platform.service
+sudo chmod 644 /etc/systemd/system/multi_module_platform.service
+sudo chmod 644 ~/multi-platform/main.py
+sudo systemctl enable multi_module_platform.service
+sudo systemctl daemon-reload
+sudo systemctl start multi_module_platform.service
