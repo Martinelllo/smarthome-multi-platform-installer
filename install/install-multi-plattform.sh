a81ddddf50015df09352e119e3f24a2d76f07256
Martinelllo <hellmann.martin@gmx.net>
2025-07-02 01:14:48 +0200

    Update installer files: Wed Jul  2 01:14:48 CEST 2025

--- install/install-multi-plattform.sh
+++ install/install-multi-plattform.sh
@@ -5,8 +5,6 @@ sudo apt-get install git -y
 
 rm -rf ~/multi-platform/
 git clone https://github.com/Martinelllo/smarthome-multi-platform-installer.git ~/multi-platform
-rm -rf ~/multi-platform/.git
-rm -rf ~/multi-platform/.bin
 
 sudo apt-get install python3-pip -y
 pip3 install -r ~/multi-platform/requirements.txt --break-system-packages
@@ -23,9 +21,11 @@ sudo raspi-config nonint do_i2c 0
 sudo apt-get install -y python3-smbus i2c-tools
 
 # install and start service
-cp ~/multi-platform/.bin/multi_module_platform.service etc/systemd/system/multi_module_platform.service
+cp ~/multi-platform/install/multi_module_platform.service etc/systemd/system/multi_module_platform.service
 sudo chmod 644 /etc/systemd/system/multi_module_platform.service
 sudo chmod 644 ~/multi-platform/main.py
 sudo systemctl enable multi_module_platform.service
 sudo systemctl daemon-reload
 sudo systemctl start multi_module_platform.service
+
+rm -rf ~/multi-platform/install
