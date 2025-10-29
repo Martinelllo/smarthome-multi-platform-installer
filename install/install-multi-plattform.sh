949596a5a32f95093f8e8c5be153cedb20cb3de9
Martinelllo <hellmann.martin@gmx.net>
2025-10-29 21:41:03 +0100

    Update installer files: Wed Oct 29 21:41:03 CET 2025

--- install/install-multi-plattform.sh
+++ install/install-multi-plattform.sh
@@ -8,18 +8,18 @@ sudo apt-get install git -y
 rm -rf $PROJECT_DIR
 git clone https://github.com/Martinelllo/smarthome-multi-platform-installer.git $PROJECT_DIR
 
-if [ ! -e "$PROJECT_DIR" ]; then
-  echo "Error: path '$PROJECT_DIR' doesn't exist." >&2
-  exit 1
-fi
+# if [ ! -e "$PROJECT_DIR" ]; then
+#   echo "Error: path '$PROJECT_DIR' doesn't exist." >&2
+#   exit 1
+# fi
 
 rm -rf $PROJECT_DIR/.git
 
 sudo apt-get install python3-pip -y
 pip3 install -r $PROJECT_DIR/install/requirements.txt --break-system-packages
 
-# install pigpiod
-sudo apt-get install pigpiod -y
+# install pigpio
+sudo apt-get install pigpio -y
 sudo systemctl enable pigpiod
 sudo systemctl start pigpiod
 
