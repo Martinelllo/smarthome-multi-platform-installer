73a0c055e1953bebd41096c58a592e9d218d8f40
Martinelllo <hellmann.martin@gmx.net>
2025-12-13 11:44:01 +0100

    Update installer files: Sat Dec 13 11:44:01 CET 2025

--- install/install-multi-plattform.sh
+++ install/install-multi-plattform.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 PROJECT_DIR=~/multi-platform
+SERVICE_DIR=/etc/systemd/system/multi_module_platform.service
 
 sudo apt-get update -y
 sudo apt-get install git -y
@@ -15,10 +16,13 @@ git clone https://github.com/Martinelllo/smarthome-multi-platform-installer.git 
 
 rm -rf $PROJECT_DIR/.git
 
-sudo apt-get install python3-pip -y
-pip3 install -r $PROJECT_DIR/install/requirements.txt --break-system-packages
+sudo chmod 644 $PROJECT_DIR/main.py
 
-# install pigpio
+# install python3 and pip
+sudo apt-get install python3-pip -y
+pip3 install -r $PROJECT_DIR/install/requirements.txt #--break-system-packages
+
+# install pigpiod
 sudo apt-get install pigpio -y
 sudo systemctl enable pigpiod
 sudo systemctl start pigpiod
@@ -29,14 +33,14 @@ sudo raspi-config nonint do_i2c 0
 # install i2c helper tools
 sudo apt-get install -y python3-smbus i2c-tools
 
+
 # install and start service
+sudo cp $PROJECT_DIR/install/multi_module_platform.service $SERVICE_DIR
 
-sudo cp $PROJECT_DIR/install/multi_module_platform.service /etc/systemd/system/multi_module_platform.service
-
-sudo chmod 644 /etc/systemd/system/multi_module_platform.service
-sudo chmod 644 ~/multi-platform/main.py
+sudo chmod 644 $SERVICE_DIR
 sudo systemctl enable multi_module_platform
 sudo systemctl daemon-reload
 sudo systemctl start multi_module_platform
 
 rm -rf $PROJECT_DIR/install
+cp -r $PROJECT_DIR/.env_dist $PROJECT_DIR/.env_dist
